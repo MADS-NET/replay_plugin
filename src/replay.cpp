9bd3694df3a1198d7221537e3ec3cdc1fbd2041d
Paolo Bosetti <paolo.bosetti@unitn.it>
2026-02-27 13:19:35 +0100

    Update to v2

--- src/replay.cpp
+++ src/replay.cpp
@@ -40,8 +40,7 @@ public:
   string kind() override { return PLUGIN_NAME; }
 
   // Implement the actual functionality here
-  return_type get_output(json &out,
-                         std::vector<unsigned char> *blob = nullptr) override {
+  return_type get_output(json &out, vector<unsigned char> *blob = nullptr) override {
     out.clear();
 
     if (!_replay->has_next()) {
@@ -66,14 +65,14 @@ public:
     return return_type::success;
   }
 
-  void set_params(void const *params) override {
+  void set_params(const json &params) override {
     // Call the parent class method to set the common parameters 
     // (e.g. agent_id, etc.)
     Source::set_params(params);
 
     // provide sensible defaults for the parameters by setting e.g.
     _params["loop"] = false;
-    _params.merge_patch(*(json *)params);
+    _params.merge_patch(params);
 
     _replay = make_unique<Replay>(_params["csv_file"]);
     _replay->set_loop(_params["loop"]);
@@ -129,7 +128,7 @@ int main(int argc, char const *argv[]) {
   params["loop"] = false;
 
   // Set the parameters
-  plugin.set_params(&params);
+  plugin.set_params(params);
 
   // Process data
   while (plugin.get_output(output) != return_type::critical) {
